baa0ba0c6c1d0d6c0549a1a1b6ef0d7be996f6bb
Nure <nalamsiddiq@gmail.com>
2025-03-08 01:21:36 +0600

    Skip DB if in test

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask
+from app.database import init_db
 
 app = Flask(__name__)
 
-if os.getenv('FLASK_ENV') == 'test':
-    app.config.from_object('app.config_test')
+if os.getenv('FLASK_ENV') != 'test':
+    init_db()
 
 # else:
 #     app.config.from_object('app.config')
