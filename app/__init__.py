fb5377deac7aa1740e35728eb4f995ef8a9fe28f
Nure <nalamsiddiq@gmail.com>
2025-03-08 00:40:55 +0600

    Remove app-config

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 if os.getenv('FLASK_ENV') == 'test':
     app.config.from_object('app.config_test')
-else:
-    app.config.from_object('app.config')
+# else:
+#     app.config.from_object('app.config')
 
 from app import routes
 
